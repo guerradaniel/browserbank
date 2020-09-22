323bc7c4a90d08098d842d2dc878b35854ee0f05
Daniel Guerra <danielguerra.g12@gmail.com>
2020-09-22 09:43:24 -0300

    Apliquei a tipagem nos arquivos TS do projeto.

--- js/controllers/NegociacaoController.js
+++ js/controllers/NegociacaoController.js
@@ -1,13 +1,12 @@
 class NegociacaoController {
     constructor() {
-        let $ = document.querySelector.bind(document);
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
+        this._inputData = document.querySelector("#data");
+        this._inputQuantidade = document.querySelector("#quantidade");
+        this._inputValor = document.querySelector("#valor");
     }
     adiciona(event) {
         event.preventDefault();
-        const negociacao = new Negociacao(this._inputData.value, this._inputQuantidade.value, this._inputValor.value);
+        const negociacao = new Negociacao(new Date(this._inputData.value.replace(/-/g, ',')), parseInt(this._inputQuantidade.value), parseFloat(this._inputValor.value));
         console.log(negociacao.quantidade + 20);
     }
 }
--- ts/controllers/NegociacaoController.ts
+++ ts/controllers/NegociacaoController.ts
@@ -1,25 +1,25 @@
 
 class NegociacaoController {
-    private _inputData
-    private _inputQuantidade
-    private _inputValor
+    private _inputData: HTMLInputElement
+    private _inputQuantidade: HTMLInputElement
+    private _inputValor: HTMLInputElement
 
     constructor() {
 
-        let $ = document.querySelector.bind(document)
-
-        this._inputData = $("#data")
-        this._inputQuantidade = $("#quantidade")
-        this._inputValor = $("#valor")
+        this._inputData = <HTMLInputElement>document.querySelector("#data")
+        this._inputQuantidade = <HTMLInputElement>document.querySelector("#quantidade")
+        this._inputValor = <HTMLInputElement>document.querySelector("#valor")
     }
 
-    adiciona(event: any) {
+    adiciona(event: Event) {
+
         event.preventDefault();
 
         const negociacao = new Negociacao(
-            this._inputData.value,
-            this._inputQuantidade.value,
-            this._inputValor.value);
+            new Date(this._inputData.value.replace(/-/g, ',')),
+            parseInt(this._inputQuantidade.value),
+            parseFloat(this._inputValor.value))
+
         console.log(negociacao.quantidade + 20)
     }
 }
